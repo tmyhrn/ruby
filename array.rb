a4d7174a6f55a52745234d385215460ac6edc8b9
EC2 Default User <ec2-user@ip-172-31-21-82.ap-northeast-1.compute.internal>
2023-05-01 08:01:59 +0000

    [finish]2-4

---
+++ array.rb
@@ -0,0 +1,11 @@
+names = ["Git", "HTML", "CSS"]
+puts names[1]
+
+names = ["Git", "HTML", "CSS"]
+puts names[2]
+
+names = ["Git", "HTML", "CSS"]
+puts names[0]
+
+names = ["Git", "HTML", "CSS"]
+puts names[3]
